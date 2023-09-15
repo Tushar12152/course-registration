8ee1c340268f1f26ed017bb9df52ad1ba2316101
Tushar12152 <tusharimran470@gmail.com>
2023-09-15 01:53:09 -0600

    remaining hour and total price added when creadit hour increase then total  hour remaining will decress

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -9,6 +9,8 @@ const Cards = () => {
     const [cards,setCards]=useState([])
     const [cart,setCart]=useState([])
     const [credit,setCredit]=useState(0)
+    const [remaining,setRemaining]=useState(20)
+    const [total,setTotal]=useState(0)
 
 // console.log(cart)
     useEffect(()=>{
@@ -24,12 +26,15 @@ const Cards = () => {
        const isTrue=cart.find(item=>item.id===card.id)
     //    isTrue?alert('selected'):setCart([...cart,card])
       let credit=card.credit
+      let total=card.price;
        if(isTrue){
          return toast.warn( 'This course is already selected',{})
        }
        else{
           cart.forEach(item=>{
             credit+=item.credit
+             total +=item.price
+
           }) 
         
         setCart([...cart,card])
@@ -38,17 +43,21 @@ const Cards = () => {
        const fixedCredit=20;
     //    credit>fixedCredit?alert('20 up'):setCredit(credit)
     if(credit>fixedCredit){
-      return  toast.warn('Total credit limit is 20')
+        return  toast.warn('Total credit limit is 20')
     }
     else{
         setCredit(credit)
+        setTotal(total)
     }
 
-         
-       
+    const remaining=fixedCredit-credit;
+    remaining<0? toast.warn('Remaining hour will not negative'):setRemaining(remaining)
+    
+    
+    
     }
 
-    console.log(credit)
+    console.log(total)
   
     return (
        <div>
@@ -64,7 +73,7 @@ const Cards = () => {
             </div>
             <div className=" w-3/12">
             <div className=" bg-white rounded-lg">
-             <h1  className="font-semibold text-center mx-auto py-4 text-[#2F80ED] border-b-2 border-gray-400 w-[80%] text-normal">Credit Hour Remaining  {} hr</h1>
+             <h1  className="font-semibold text-center mx-auto py-4 text-[#2F80ED] border-b-2 border-gray-400 w-[80%] text-normal">Credit Hour Remaining  {remaining} hr</h1>
              <h1  className="font-semibold  mx-auto py-4 text-black border-b-2 border-gray-400 w-[80%] text-normal">Course Name</h1>
                 
                    
@@ -75,7 +84,7 @@ const Cards = () => {
                  }
                  <ToastContainer></ToastContainer>
              <h1  className="font-normal  mx-auto py-4 text-black border-b-2 border-gray-400 w-[80%] text-normal">Total Credit Hour : {credit}</h1>
-             <h1 className="font-normal  mx-auto py-4 text-black  w-[80%] text-normal">Total Price : {} USD</h1>
+             <h1 className="font-normal  mx-auto py-4 text-black  w-[80%] text-normal">Total Price : {total} USD</h1>
             </div>
             </div>
         </div>
